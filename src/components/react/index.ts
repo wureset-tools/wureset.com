466ca6469606989f826f03664c3183fce22d9068
ManuelGil <atencion.ms@outlook.com>
2023-04-10 02:47:19 -0500

    style: :lipstick: remove relative and negative styles

--- src/components/react/index.ts
+++ src/components/react/index.ts
@@ -1,6 +1,5 @@
 export { default as ActionButtons } from './ActionButtons';
 export { default as Buttons } from './Buttons';
 export * from './Footer';
-export * from './Navbar';
 export * from './Theme';
 export * from './Utils';
