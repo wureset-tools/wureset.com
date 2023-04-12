76b85d01f7c5a2d8db490ac51caabd0cfd8dd1b6
Zyruks <kuzenshi@gmail.com>
2023-04-12 04:53:21 -0300

    chore: :wrench: update react/index.ts , delete footer react component

--- src/components/react/index.ts
+++ src/components/react/index.ts
@@ -1,5 +1,4 @@
 export { default as ActionButtons } from './ActionButtons';
 export { default as Buttons } from './Buttons';
-export * from './Footer';
 export * from './Theme';
 export * from './Utils';
